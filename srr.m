dc049adb13647c55ab4d476e81857e990087230f
Robert Johnstone <r.johnstone.17@ucl.ac.uk>
2019-07-24 11:17:44 +0100

    Added dummy code for SR recon, plus some tidying

--- srr.m
+++ srr.m
@@ -10,33 +10,32 @@ close all
 phantom_radius = 100; % mm
 
 % Acquisition parameters
-fov = 300; % mm, governs number of slices, too
+fov = 304; % mm, governs number of slices, too
 slice_thickness = 10; % mm
-slice_spacing = 2; % mm
-acq_resn = 2; % mm, in-slice resolution
+slice_spacing = 4; % mm - must give even number of pixels in slice
+acq_resn = 4; % mm, in-slice resolution
 slice_profile = 'gaussian';
 
 % Simulation parameters
 sim_resn = 0.1; % mm
 disp_resn = 0.25; % mm
-kernel_width = 40; % mm, full width of kernel, not slice width
 
 % Display options
-interp = 'nearest'; % Can be a cell array representing a blurring kernel
+interp = 'cubic'; % Can be a cell array representing a blurring kernel
 
 % Derived parameters
-n_y_pts = (fov/sim_resn)+1;
-n_x_pts = (fov/sim_resn)+1;
-y = linspace(-fov/2,+fov/2,n_y_pts);
-x = linspace(-fov/2,+fov/2,n_x_pts);
+sim_y_pts = (fov/sim_resn)+1;
+sim_x_pts = (fov/sim_resn)+1;
+y = linspace(-fov/2,+fov/2,sim_y_pts);
+x = linspace(-fov/2,+fov/2,sim_x_pts);
+acq_y_pts = (fov/acq_resn)+1;
+acq_x_pts = (fov/acq_resn)+1;
+slices = (fov/slice_spacing)+1;
 
-% Define blurring kernel
-n_kernel_pts = (kernel_width/sim_resn)+1;
-kernel_pts = linspace(-kernel_width/2,+kernel_width/2,n_kernel_pts);
+% Set up parameters for kernel
 switch slice_profile
     case 'gaussian'
         sigma = slice_thickness/(2*sqrt(2*log(2)));
-        kernel = exp(-((kernel_pts/sigma).^2)/2)/(sigma*sqrt(2*pi));
 end
 
 % Define phantom
@@ -44,10 +43,7 @@ end
 phantom=(X.^2+Y.^2)<phantom_radius^2;
 
 % Iterate through the slices, exciting a slice and acquiring
-slices = (fov/slice_spacing)+1;
 slices_y = linspace(-fov/2,+fov/2,slices);
-n_y_pts = (fov/sim_resn)+1;
-y = linspace(-fov/2,+fov/2,n_y_pts);
 img = zeros(fov/acq_resn+1,slices);
 for slice = 1:slices
     % Excite phantom
@@ -59,21 +55,36 @@ for slice = 1:slices
     excitation = repmat(kernel_shifted,(fov/sim_resn)+1,1);
     phant_excited = phantom.*excitation;
 %     imshow(phant_excited',[0 .1]);
-    pause(.01)
+%     pause(.01)
     % Acquire echo
     echo = fft(sum(phant_excited,2));
     % Truncate echo to number of acquired samples - fov/acq_resn+1
-    echo_truncated = [echo(1:fov/(2*acq_resn)+1); echo(n_x_pts-fov/(2*acq_resn)+1:end)];
+    echo_truncated = [echo(1:fov/(2*acq_resn)+1); echo(sim_x_pts-fov/(2*acq_resn)+1:end)];
     % Reconstruct slice
     image_slice = abs(ifft(echo_truncated));
-%     plot(image_slice)
     % Store slice in 2D image
     img(:,slice) = image_slice;
 end
 
 % Display acquired image
-img_disp = imresize(img,[(acq_resn/disp_resn)*size(img,1),(slice_spacing/disp_resn)*size(img,2)],'nearest');
+img_disp = imresize(img,[(acq_resn/disp_resn)*size(img,1),(slice_spacing/disp_resn)*size(img,2)],interp);
 imshow(img_disp',[])
 title('Acquired image -- nearest-neighbour interpolation', 'Interpreter', 'latex')
 xlabel('$x$ -- in-slice', 'Interpreter', 'latex');
 ylabel('$y$ -- through-slice', 'Interpreter', 'latex');
+
+% Perform SRR in through-slice (y) direction
+srr_img = zeros(size(img));
+kernel_width = sqrt(slice_thickness^2-acq_resn^2);
+for column_x = 1:acq_x_pts
+    srr_img(column_x,:) = srrecon(img(column_x,:),'gaussian',kernel_width);
+end
+
+% Display SR reconstructed image
+img_disp = imresize(srr_img,[(acq_resn/disp_resn)*size(img,1),(slice_spacing/disp_resn)*size(img,2)],interp);
+figure
+imshow(img_disp',[])
+title('SRR image -- nearest-neighbour interpolation', 'Interpreter', 'latex')
+xlabel('$x$ -- in-slice', 'Interpreter', 'latex');
+ylabel('$y$ -- through-slice', 'Interpreter', 'latex');
+
---
+++ srrecon.m
@@ -0,0 +1,7 @@
+function [output_image] = srrecon(input_image,kernel_type,kernel_width)
+%SRRECON Performs a super-resolution reconstruction
+%   Takes a 1D image vector and, using a specified kernel_type, performed a
+%   super-resolution reconstruction
+    output_image = input_image;
+end
+
