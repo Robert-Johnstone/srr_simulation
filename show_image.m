263ac65967102f07d81f3be4e16964659d5d0f06
Robert Johnstone <r.johnstone.17@ucl.ac.uk>
2019-07-26 12:37:27 +0100

    Added inserts to circular phantom

--- show_image.m
+++ show_image.m
@@ -3,7 +3,7 @@ function [outputArg1,outputArg2] = show_image(img,disp_size,interp,img_title,sca
 
     img_disp = imresize(img,disp_size,interp);
     figure
-    imshow(img_disp',[])
+    imshow(flipud(img_disp),[])
     title(img_title, 'Interpreter', 'latex')
     xlabel('$x$ -- in-slice', 'Interpreter', 'latex');
     ylabel('$y$ -- through-slice', 'Interpreter', 'latex');
--- srr.m
+++ srr.m
@@ -14,10 +14,10 @@ fov = 300; % mm - must be even multiple of slice spacing
 slice_thickness = 6; % mm
 slice_spacing = 2; % mm - must divide fov to give even number
 acq_resn = 2; % mm, in-slice resolution
-slice_profile = 'sinc'; % gaussian, rect, rect_adv, sinc
+slice_profile = 'gaussian'; % gaussian, rect, rect_adv, sinc
 
 % Simulation parameters
-sim_resn = 0.1; % mm
+sim_resn = 0.2; % mm
 
 % SRR parameters
 % Project kernel width in y pixels (units of slice spacing)
@@ -25,7 +25,7 @@ kernel_width = sqrt(slice_thickness^2-slice_spacing^2)/slice_spacing; % The 'rig
 % kernel_width = slice_thickness/slice_spacing; % The 'wrong' width
 
 % Display options
-interp = 'nearest'; % Can be a cell array representing a blurring kernel
+interp = 'cubic'; % Can be a cell array representing a blurring kernel
 disp_resn = 0.5; % mm
 
 % Derived parameters
@@ -39,7 +39,26 @@ slices = (fov/slice_spacing)+1;
 
 % Define phantom
 [X,Y] = meshgrid(x,y);
-phantom=double((X.^2+Y.^2)<phantom_radius^2);
+phantom = double((X.^2+Y.^2)<phantom_radius^2);
+% Add rectangular insert
+rect_centre = [-30 40];
+rect_height = 40; % mm
+rect_width = 40; % mm
+rect_angle = 45; % mm
+phantom(and(abs((X*cosd(rect_angle)-Y*sind(rect_angle))-rect_centre(1))<(rect_width/2), ...
+    abs((Y*cosd(rect_angle)+X*sind(rect_angle))-rect_centre(2))<(rect_height/2))) = 0;
+% Add ellipsoid
+elip_centre = [40 -40];
+elip_height = 25; % mm
+elip_width = 60; % mm
+phantom((((X-elip_centre(1)).^2)./(elip_width/2)^2) + ...
+    (((Y-elip_centre(2)).^2)./(elip_height/2)^2) < 1) = 0.2;
+% Add circle
+circ_centre = [-40 -20];
+circ_diam = 40; % mm
+phantom(((X-circ_centre(1)).^2) + ...
+    ((Y-circ_centre(2)).^2) < ((circ_diam/2)^2)) = 0.4;
+show_image(phantom,[500 500],'cubic','Phantom',0)
 
 % Acquire MR image
 img = mri_acq(phantom,fov,sim_resn,acq_resn,slice_thickness,slices,slice_profile,y);
