574d3781309c55297db86b155e2d0f4daa5bf36e
Robert Johnstone <r.johnstone.17@ucl.ac.uk>
2019-08-02 10:33:24 +0100

    Removed commented code from create_fp_kernel.m

--- create_fp_kernel.m
+++ create_fp_kernel.m
@@ -8,7 +8,7 @@ function fp_kernel = create_fp_kernel(fp_kernel_size)
 %   fp_kernel_size - size of kernel array (integer)
 
     % Set number of iterations for iterative back projection
-    max_iter = 1000;
+    max_iter = 100;
     
     % Switch to turn plotting errors on and off
     plot_errors = 0;
@@ -20,17 +20,8 @@ function fp_kernel = create_fp_kernel(fp_kernel_size)
         plot(0,norm(fp_kernel-ground_truth),'x')
         hold on
     end
-%     last_error = Inf;
 
     % Create forward projection kernel
-%     m = 3; % Multiplier used to set how big vector representing kernel is
-%     n_kernel_pts = ceil(fp_width)*m+1;
-%     % Make sure kernel and image either both have an odd number or both
-%     % have an even number of pixels. This avoids conv function introducing 
-%     % a shift
-%     if mod(n_kernel_pts,2)~=mod(size(lr_image,2),2)
-%         n_kernel_pts = n_kernel_pts+1;
-%     end
     kernel_pts = linspace(-(fp_kernel_size-1)/2,(fp_kernel_size-1)/2,fp_kernel_size);
     % Load saved slice profile and resample of kernel point spacing
     load('sg_150_100_167.mat','profile');
@@ -45,10 +36,6 @@ function fp_kernel = create_fp_kernel(fp_kernel_size)
     % Create initial high resultion image guess
     fp_kernel = slice_profile_lr;
 
-%     figure
-%     plot(slice_profile_lr,'+')
-%     hold on
-    
     for i = 1:max_iter
         % Forward project using HR slice profile
         slice_profile_lr_guess = conv(fp_kernel,slice_profile_hr,'same');
@@ -57,18 +44,6 @@ function fp_kernel = create_fp_kernel(fp_kernel_size)
         % Back project error using HR slice profile
         slice_profile_hr_error = conv(slice_profile_lr_error,slice_profile_hr,'same');
         fp_kernel = fp_kernel-slice_profile_hr_error;
-%         % Calculate error metric and bail out if it is getting bigger
-%         current_error = norm(fp_kernel_temp-ground_truth);
-%         if current_error > last_error
-%             break
-%         else
-%             last_error = current_error;
-%             fp_kernel = fp_kernel_temp;
-%         end
-%         if plot_errors
-%             plot(i,norm(fp_kernel-ground_truth),'x')
-%         end
-%      plot(slice_profile_lr_guess)
     end
     % Normalise
     fp_kernel = fp_kernel/sum(fp_kernel);
