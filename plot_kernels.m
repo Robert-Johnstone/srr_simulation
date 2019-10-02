db68580567071c6768f7ec039bcf2b860224362c
Robert Johnstone <r.johnstone.17@ucl.ac.uk>
2019-10-02 16:50:27 +0100

    Tidied up plot_kernels.m and overlaid the four kernels on each plot

--- plot_kernels.m
+++ plot_kernels.m
@@ -1,51 +1,78 @@
+clear 
+close all
+
+% Create figures for later use
+fig_profile = figure;
+fig_spectrum = figure;
+fig_profile.Position(3:4) = [560 210];
+fig_spectrum.Position(3:4) = [560 210];
+
+save_images = 1;
+
 %   fp_kernel_type - FP kernel type, e.g. 'gaussian',<filename>
 %   fp_width - the nominal width of the kernel, e.g. FWHM, in pixels
 %                   (units of slice spacing)
 slice_thickness = 6; % mm
 slice_spacing = 2; % mm
-fp_kernel_type = 'generated';
+
+fp_kernel_type = 'gaussian';
+fp_width = slice_thickness/slice_spacing; % The 'wrong' width
+plot_curves(slice_spacing,fp_kernel_type,fp_width,fig_profile,fig_spectrum,'k','-')
+
+fp_kernel_type = 'gaussian';
 fp_width = sqrt(slice_thickness^2-slice_spacing^2)/slice_spacing; % The 'right' width
+plot_curves(slice_spacing,fp_kernel_type,fp_width,fig_profile,fig_spectrum,'k','--')
 
-% Create forward projection kernel
-m = 3; % Multiplier used to set how big vector representing kernel is
-n_kernel_pts = ceil(fp_width)*m;
-% Make sure kernel size is odd
-if mod(n_kernel_pts,2)==0
-    n_kernel_pts = n_kernel_pts + 1;
+fp_kernel_type = 'generated';
+plot_curves(slice_spacing,fp_kernel_type,fp_width,fig_profile,fig_spectrum,'k',':')
+
+fp_kernel_type = 'sg_150_100_167.mat';
+fp_width = sqrt(slice_thickness^2-slice_spacing^2)/slice_spacing; % The 'right' width
+plot_curves(slice_spacing,fp_kernel_type,fp_width,fig_profile,fig_spectrum,'k','-.')
+
+if save_images
+    figure(fig_profile)
+    saveas(gcf,'kernels','epsc')
+    figure(fig_spectrum)
+    saveas(gcf,'kernels_ft','epsc')
 end
 
-kernel_pts = linspace(-(n_kernel_pts-1)/2,(n_kernel_pts-1)/2,n_kernel_pts);
-switch fp_kernel_type
-    case 'gaussian'
-        sigma = fp_width/(2*sqrt(2*log(2)));
-        fp_kernel = exp(-((kernel_pts/sigma).^2)/2)/(sigma*sqrt(2*pi));
-    case 'generated'
-        fp_kernel = create_fp_kernel(n_kernel_pts);
-    otherwise
-        % Load saved profile
-        load(fp_kernel_type,'profile');
-        spw = 6; % Conventional slice width for saved slice profile, mm
-        spr = 0.001; % Conventional resolution for saved slice profile, mm
-        fp_kernel = interp1((-0.24:1e-6:0.24)*fp_width/spw,profile,kernel_pts*spr,'linear',0);
-        % Normalise
-        fp_kernel = fp_kernel/sum(fp_kernel);
-end
+function plot_curves(slice_spacing,fp_kernel_type,fp_width,fig_profile,fig_spectrum,linecol,markers)
+    % Create forward projection kernel
+    m = 3; % Multiplier used to set how big vector representing kernel is
+    n_kernel_pts = ceil(fp_width)*m;
+    % Make sure kernel size is odd
+    if mod(n_kernel_pts,2)==0
+        n_kernel_pts = n_kernel_pts + 1;
+    end
 
-% Compare central lines profiles
-fig = figure;
-plot(kernel_pts,fp_kernel,'k-')
-xlabel('y / pixels','Interpreter','latex')
-% ax = gca;
-% ax.YLim = [-0.05 1.05];
-% fig.Position(3:4) = [560 210];
-% if save_images
-%     saveas(gcf,[fn_root 'profiles'],'epsc')
-% end
-% Plot Fourier transform
-fig = figure;
-plotSpectrum(fp_kernel,n_kernel_pts*slice_spacing/1000,'FT',...
-    [0 500/slice_spacing],[-100 0],1,'k','--')
-% fig.Position(3:4) = [560 210];
-% if save_images
-%     saveas(gcf,[fn_root 'profiles_ft'],'epsc')
-% end
+    kernel_pts = linspace(-(n_kernel_pts-1)/2,(n_kernel_pts-1)/2,n_kernel_pts);
+    switch fp_kernel_type
+        case 'gaussian'
+            sigma = fp_width/(2*sqrt(2*log(2)));
+            fp_kernel = exp(-((kernel_pts/sigma).^2)/2)/(sigma*sqrt(2*pi));
+        case 'generated'
+            fp_kernel = create_fp_kernel(n_kernel_pts);
+        otherwise
+            % Load saved profile
+            load(fp_kernel_type,'profile');
+            spw = 6; % Conventional slice width for saved slice profile, mm
+            spr = 0.001; % Conventional resolution for saved slice profile, mm
+            fp_kernel = interp1((-0.24:1e-6:0.24)*fp_width/spw,profile,kernel_pts*spr,'linear',0);
+            % Normalise
+            fp_kernel = fp_kernel/sum(fp_kernel);
+    end
+
+    % Compare central lines profiles
+    figure(fig_profile)
+    plot(kernel_pts,fp_kernel,'Color',linecol,'LineStyle',markers)
+    hold on
+    xlabel('z / pixels','Interpreter','latex')
+
+    % Plot Fourier transform
+    figure(fig_spectrum)
+    plotSpectrum(fp_kernel,n_kernel_pts*slice_spacing/1000,'FT',...
+        [0 500/slice_spacing],[-100 0],0,linecol,markers)
+    ax = gca;
+    ax.YLim = [0 1];
+end
